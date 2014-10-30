f78260adb5ba9fe2cf5a0fc2afd5eed019789e02
mamt84 <morcate.martin@yahoo.com>
2014-10-29 22:05:44 -0400

    started to create offer

---
+++ src/main/java/com/aguacate/send2cuba/restful/model/offer/Offer.java
@@ -0,0 +1,9 @@
+package com.aguacate.send2cuba.restful.model.offer;
+
+import com.aguacate.send2cuba.restful.model.base.PersistentEntity;
+
+public abstract class Offer extends PersistentEntity {
+
+	private static final long serialVersionUID = -2010632110634176528L;
+
+}
